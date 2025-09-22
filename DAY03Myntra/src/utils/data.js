c5aaa298c3aa2bd809e020cbf998e7264ac11957
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-09-22 22:35:30 +0530

    Created dummy data for Card

---
+++ DAY03Myntra/src/utils/data.js
@@ -0,0 +1,32 @@
+const arr = [
+    {
+        clothName: "Ethnic Wear",
+        Offer: "50-80% OFF"
+    },
+    {
+        clothName: "Shoes",
+        Offer: "10-30% OFF"
+    },
+    {
+        clothName: "Skirts",
+        Offer: "10-20% OFF"
+    },
+    {
+        clothName: "Shirts",
+        Offer: "30-50% OFF"
+    },
+    {
+        clothName: "Kurta",
+        Offer: "15-45% OFF"
+    },
+    {
+        clothName: "Bags",
+        Offer: "50-80% OFF"
+    },
+    {
+        clothName: "Bottle",
+        Offer: "10-15% OFF"
+    },
+]
+
+export default arr;
