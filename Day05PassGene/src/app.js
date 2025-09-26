ed81a14bedad439bcf8d7200c74d85e4525f1807
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-09-26 23:43:08 +0530

    Added functionality for random password generating

--- Day05PassGene/src/app.js
+++ Day05PassGene/src/app.js
@@ -9,24 +9,48 @@ function App()
     const [includeNumber, setIncludeNumber] = useState(false)
     const [includeCharacter, setIncludeCharacter] = useState(false)
 
+    function generatePassword()
+    {
+        let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+        if(includeNumber)
+        str+="0123456789"
+
+        if(includeCharacter)
+        str+="/?><(){[]}!@#$%^&*-==_+|";
+
+        let pass = ""
+
+        for(let i=0; i<length; i++)
+        {
+            pass+= str[Math.floor(Math.random() * str.length)]
+        }
+
+        setPassword(pass);
+    }
+
+    useEffect(()=>{
+         generatePassword()
+    }, [length, includeCharacter, includeNumber])
+   
 
 
     return <>
         <h1>Your Password: {password}</h1>
         <div className="inputs">
             <div className="length">
-            <input type="range" name="password-length" id="length" value={length}/>
+            <input type="range" name="password-length" id="length" value={length} onChange={(e)=>{setLength(e.target.value)}}/>
             <label htmlFor="length">Length: {length}</label>
             </div>
 
 
             <div>
-            <input type="checkbox" name="" id="number" defaultChecked={includeNumber}/>
+            <input type="checkbox" name="" id="number" defaultChecked={includeNumber} onChange={()=>{setIncludeNumber(!includeNumber)}}/>
             <label htmlFor="number">Number</label>
             </div>
 
             <div>
-            <input type="checkbox" name="" id="characters" defaultChecked={includeCharacter}/>
+            <input type="checkbox" name="" id="characters" defaultChecked={includeCharacter} onChange={()=>{setIncludeCharacter(!includeCharacter)}}/>
             <label htmlFor="characters">Character</label>
             </div>
         </div>
