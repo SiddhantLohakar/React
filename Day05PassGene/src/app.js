3d60d06a0a933e80bc90e76ea3dbc93f01e65a9e
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-09-26 22:52:33 +0530

    Initialized project for random password generator

---
+++ Day05PassGene/src/app.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+import ReactDOM from "react-dom/client"
+
+function App()
+{
+    return <>
+        <h1>Hello World</h1>
+    </>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App/>);
