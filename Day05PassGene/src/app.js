2896a5bf967a27d560f96eb0372c8f005b12144b
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-09-26 23:24:13 +0530

    Created a basic UI for password generator

--- Day05PassGene/src/app.js
+++ Day05PassGene/src/app.js
@@ -1,10 +1,30 @@
 import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client"
+import "./app.css"
 
 function App()
 {
+    const [passoword, setPassword] = useState("");
+
     return <>
-        <h1>Hello World</h1>
+        <h1>Your Password: {passoword}</h1>
+        <div className="inputs">
+            <div className="length">
+            <input type="range" name="password-length" id="length" />
+            <label htmlFor="length">Length: {length}</label>
+            </div>
+
+
+            <div>
+            <input type="checkbox" name="" id="number" />
+            <label htmlFor="number">Number</label>
+            </div>
+
+            <div>
+            <input type="checkbox" name="" id="characters" />
+            <label htmlFor="characters">Character</label>
+            </div>
+        </div>
     </>
 }
 
