3eb5afb368a1d764bf041b1dbbda1e354dc72fc8
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-09-30 23:15:20 +0530

    Initialized folder for more hooks

---
+++ Day07MoreHooks/src/app.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+
+
+const ReactRoot = ReactDOM.createRoot(document.getElementById("root"));
+ReactRoot.render(<h1>Hello World</h1>)
