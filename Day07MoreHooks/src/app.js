0262ab9dc9ea57174ac49718f3cf1958e900ccdf
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-10-01 09:39:59 +0530

    Added useRef and useMemo hook

--- Day07MoreHooks/src/app.js
+++ Day07MoreHooks/src/app.js
@@ -1,7 +1,40 @@
-import React from "react"
+import  {useMemo, useRef, useState } from "react"
 import ReactDOM from "react-dom/client"
 
 
+ function calcFibonacci(number)
+    {
+        if(number <= 1)
+            return number;
+
+
+        return calcFibonacci(number-2) + calcFibonacci(number - 1)
+    }
+
+function App()
+{
+
+    const [count, setCount] = useState(0);
+    const [number, setNumber] = useState(0)
+    const money = useRef(0);
+
+    let result = useMemo(()=>calcFibonacci(number), [number])
+
+    return (
+        <>
+            <h1>Counter is {count}</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>Decrement</button>
+            <br/>
+            <br/>
+            <h2>Fibonacci of the number {number} is: {result} </h2>
+            <input type="number" value={number} onChange={(e)=>{setNumber(e.target.value)}}/>
+            <h3>Money is {money.current}</h3>
+             <button onClick={()=>money.current+=1}>Increment</button>
+        </>
+    )
+}
+
 
 const ReactRoot = ReactDOM.createRoot(document.getElementById("root"));
-ReactRoot.render(<h1>Hello World</h1>)
+ReactRoot.render(<App/>)
