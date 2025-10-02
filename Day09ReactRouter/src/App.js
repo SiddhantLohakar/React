26caaa1c48a57c9b52896bba5e2e60bdf585e1ef
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-10-02 21:09:42 +0530

    Initialized the react-router folder

---
+++ Day09ReactRouter/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+function App()
+{
+    return (
+        <h1>Hello Siddhant</h1>
+    )
+}
+
+const ReactRoot = ReactDOM.createRoot(document.getElementById("root"));
+ReactRoot.render(<App/>)
